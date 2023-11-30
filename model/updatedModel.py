877b8cecb90ba7e54427c30bb178a29f4f558e88
Warin <smbm2003@hotmail.com>
2023-11-30 15:12:11 -0700

    test loop made

--- model/updatedModel.py
+++ model/updatedModel.py
@@ -442,3 +442,56 @@ def train(regression_on, dataloader, model, loss_function, optimizer, device_in_
         avg_rmse = total_rmse/len(dataloader)
 
         return avg_loss, avg_rmse
+
+def test(regression_on, dataloader, model, loss_function, device_in_use):
+    model.eval()
+
+    total_loss=0
+    total_correct_1 = 0
+    total_samples_1 = 0
+    all_targets_1 = []
+    all_predictions_1 = []
+
+    total_rmse = 0
+
+    if not regression_on:
+        with torch.no_grad():
+            for (cat_x, cont_x, labels) in dataloader:
+                cat_x,cont_x,labels=cat_x.to(device_in_use),cont_x.to(device_in_use),labels.to(device_in_use)
+                print(cont_x.shape)
+
+                predictions = model(cat_x, cont_x)
+
+                loss = loss_function(predictions, labels.long())
+                total_loss+=loss.item()
+
+                #computing accuracy
+                y_pred_softmax_1 = torch.softmax(predictions, dim=1)
+                _, y_pred_labels_1 = torch.max(y_pred_softmax_1, dim=1)
+                total_correct_1 += (y_pred_labels_1 == labels).sum().item()
+                total_samples_1 += labels.size(0)
+                all_targets_1.extend(labels.cpu().numpy())
+                all_predictions_1.extend(y_pred_labels_1.cpu().numpy())
+
+            avg_loss = total_loss/len(dataloader)
+            accuracy = total_correct_1 / total_samples_1
+
+            return avg_loss, accuracy
+    
+    else:
+        with torch.no_grad():
+            for (cat_x, cont_x, labels) in dataloader:
+                cat_x,cont_x,labels=cat_x.to(device_in_use),cont_x.to(device_in_use),labels.to(device_in_use)
+
+                predictions = model(cat_x, cont_x)
+
+                loss = loss_function(predictions, labels.unsqueeze(1))
+                total_loss+=loss.item()
+
+                rmse_value = rmse(labels.unsqueeze(1), predictions)
+                total_rmse+=rmse_value
+
+            avg_loss = total_loss/len(dataloader)
+            avg_rmse = total_rmse/len(dataloader)
+
+            return avg_loss, avg_rmse
